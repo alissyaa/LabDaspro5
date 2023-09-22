f2e5e8fc77d584d2e44e6698e1d41ee42d89d706
alissyaa <alisssyahumairah@gmail.com>
2023-09-22 09:43:57 +0700

    kuis 1

---
+++ tugas1.pas
@@ -0,0 +1,16 @@
+program sapa;
+var
+nama, nim, kom, alamat :string;
+begin
+write('Masukkan Nama Anda: ');
+readln(nama);
+write('Masukkan NIM Anda: ');
+readln(nim);
+write('Masukkan KOM Anda: ');
+readln(kom);
+write('Masukkan Alamat Anda: ');
+readln(alamat);
+
+writeln('Hallo, Nama saya ',nama, ', NIM saya ',nim, ', saya di kom ',kom);
+writeln('alamat saya di ', alamat, '. Salam kenal semua!');
+end.
